766c7b8cb973acb5aa0aa1e9808531a2d082bc4c
showyourwork <showyourwork>
2022-08-14 18:07:42 +0200

    [showyourwork] overleaf sync

---
+++ src/scripts/asassn21qj.py
@@ -0,0 +1,8 @@
+import numpy as np
+from astropy import constants as c
+from astropy import units as u
+
+star_rad = 1.01 * u.Rsun
+star_mass = 1.2 * u.Msun
+star_lum = 1.1 * u.Lsun
+star_dist = 560 * u.parsec
