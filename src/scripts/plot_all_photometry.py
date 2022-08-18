2a096293839364d53bdaceb0008094454fa8b418
showyourwork <showyourwork>
2022-08-18 05:52:31 +0200

    [showyourwork] overleaf sync

--- src/scripts/plot_all_photometry.py
+++ src/scripts/plot_all_photometry.py
@@ -3,26 +3,61 @@ import matplotlib.pyplot as plt
 from astropy.io import ascii
 import paths
 
+import matplotlib as mpl
+mpl.rcParams.update({'font.size': 14})
+
 twi = ascii.read(paths.data / 'obs_NEOWISE.ecsv')
 tat = ascii.read(paths.data / 'obs_ATLAS.ecsv')
 tas = ascii.read(paths.data / 'obs_ASASSN.ecsv')
 
+tasg = tas[tas['Filter']=='g']
+tasV = tas[tas['Filter']=='V']
+
 fig, (ax1,ax2,ax3) = plt.subplots(3,1,figsize = (10,7), sharex=True)
 
 
-ax2.errorbar(twi['MJD'],twi['w1'],yerr=twi['w1err'],color='blue',fmt='.',label='W1')
-ax2.errorbar(twi['MJD'],twi['w2'],yerr=twi['w2err'],color='red', fmt='.',label='W2')
-ax3.errorbar(twi['MJD'],twi['w1w2'],yerr=twi['w1w2err'],fmt='.')
+ax2.errorbar(twi['MJD'],twi['w1'],yerr=twi['w1err'],
+    color='blue',fmt='.',
+    ms=10,
+    label='W1')
+ax2.errorbar(twi['MJD'],twi['w2'],yerr=twi['w2err'],
+    color='red', fmt='.',
+    ms=10,
+    label='W2')
+ax3.errorbar(twi['MJD'],twi['w1w2'],yerr=twi['w1w2err'],
+    ms=10,
+    fmt='.')
 
 
-ax1.errorbar(tat['MJD'],tat['fnorm'],yerr=tat['fnormerr'],color='blue',fmt='.', label='ATLAS')
-ax1.errorbar(tas['MJD'],tas['fnorm'],yerr=tas['fnormerr'],color='green',fmt='.', label='ASASSN')
+#ax1.errorbar(tat['MJD'],tat['fnorm'],yerr=tat['fnormerr'],color='blue',fmt='.', label='ATLAS')
+#ax1.errorbar(tas['MJD'],tas['fnorm'],yerr=tas['fnormerr'],
+#    color='green',fmt='.',
+#    alpha=0.5,
+#    ms=10,
+#     label='ASASSN')
+ax1.scatter(tasg['MJD'],tasg['fnorm'],
+    color='green',
+    alpha=0.2,
+    s=10,
+    edgecolors='none',
+    label='ASASSN g\'')
+ax1.scatter(tasV['MJD'],tasV['fnorm'],
+    color='blue',
+    alpha=0.2,
+    s=10,
+    edgecolors='none',
+    label='ASASSN V')
 
 ax1.set_ylim(0,1.1)
 ax2.set_ylim(12,10.5)
-ax1.set_xlim(56000,59800)
+ax1.set_xlim(56600,59800)
 ax1.legend()
 ax2.legend()
 ax3.set_xlabel('Epoch [MJD]')
+ax1.set_ylabel('Magnitude')
+ax2.set_ylabel('Magnitude')
+ax3.set_ylabel('Color (W1-W2)')
 
-plt.savefig(paths.figures / 'all_photometry.pdf')
+plt.savefig(paths.figures / 'all_photometry.png',
+    bbox_inches='tight',
+    dpi=200)
